4da2b8d42444cc8093dc34d05af4ec2578599630
temir-cs <temirlan.akhanov@yandex.ru>
2020-10-04 19:03:17 +0600

    feat: solved transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const instructions = ['--discard-prev', '--double-prev', '--discard-next', '--double-next'];
+
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error(`${arr} is not a valid array!`);
+  }
+
+  const discard = (array, phrase) => {
+    const direction = phrase.substr(phrase.length - 4);
+    let result = array.slice();
+    const index = array.indexOf(phrase);
+    direction === 'prev'
+    ? result[index - 1] = ''
+    : result[index + 1] = '';
+    result[index] = '';
+    return result;
+  }
+  const double = (array, phrase) => {
+    const direction = phrase.substr(phrase.length - 4);
+    let result = array.slice();
+    const index = array.indexOf(phrase);
+    result[index] = direction === 'prev' ? result[index - 1] : result[index + 1];
+    return result;
+  }
+
+  const result = arr.reduce((acc, current) => {
+    switch (current) {
+      case '--discard-prev':
+        return discard(acc, current);
+      case '--discard-next':
+        return discard(acc, current);
+      case '--double-prev':
+        return double(acc, current);
+      case '--double-next':
+        return double(acc, current);
+      default:
+        return acc;
+    }
+  }, arr);
+  return result.filter((item) => item !== '' && item !== undefined);
 };
